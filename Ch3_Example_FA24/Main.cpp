056c17c60b0ee7fdc0bd32e27fe6bbd8b11a8409
BuschMath <busch@iowacentral.edu>
2024-09-16 09:52:56 -0500

    type casting

--- Ch3_Example_FA24/Main.cpp
+++ Ch3_Example_FA24/Main.cpp
@@ -7,5 +7,11 @@ int main()
 	std::cout << x << std::endl;
 	std::cout << x + 1 << std::endl;
 
+	std::cout << 'G' / 'A' << std::endl; // 'G' = 71, 'A' = 65, 71 / 65 = 1
+	std::cout << 'G' % 'A' << std::endl; // 71 % 65 = 6
+
+	std::cout << (81 + 32) / 2 << std::endl;
+	std::cout << char(81 + 32) / 2 << std::endl;
+
 	return 0;
 }
